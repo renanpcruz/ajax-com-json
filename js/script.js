ea52aa34bc5ba043b8c4fc8cbb089691300463dd
Renan Cruz <renanpc61@gmail.com>
2023-11-21 15:33:09 -0300

    avanço sinistro (eu acho)

--- js/script.js
+++ js/script.js
@@ -1,26 +1,37 @@
-$('form').submit(function(){
+$('form1').submit(function (e) { 
+    e.preventDefault();
+    
+    
+    var u_name = $('#name').val();
+    var u_email = $('#email').val();
 
-    var _name =  $('#name').val();
-    var _email =  $('#email').val();
-
-    //console.log(_email, _name);
+    //console.log(u_email, u_name);
 
     $.ajax({
-        url: 'https://epansani.com.br/2023/dw1s4/ajax/ins.php',
         method: 'POST',
-        data: {nome: _name, email: _email},
-        dataType: 'json'
+        url: 'https://epansani.com.br/2023/dw1s4/ajax/ins.php',
+        data: {nome: u_name, email: u_email},
+        dataType: "json"
     }).done(function(result){
+        $('name').val('');
+        $('email').val('');
         console.log(result);
     });
 });
 
 function getEmail() {
     $.ajax({
-        url: 'https://epansani.com.br/2023/dw1s4/ajax/list.php',
         method: 'GET',
-        dataType: 'json'
+        url: 'https://epansani.com.br/2023/dw1s4/ajax/list.php',
+        dataType: "json"
     }).done(function(result){
         console.log(result);
+        
+        for(var i = 0; i<result.lenght; i++){
+            $('#table-body').prepend('<tr><td>Mark</td><td>Otto</td><td><button class="btn btn-danger">Apagar</button></td></tr>');
+        }
+
     });
 }
+
+getEmail();
